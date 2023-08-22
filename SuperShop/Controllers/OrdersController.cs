e789e194722413a2e7077b528f0e21b1dee6815f
marianaleitt <marianaleitt@gmail.com>
2023-08-22 11:24:26 +0100

    ComboBox

--- SuperShop/Controllers/OrdersController.cs
+++ SuperShop/Controllers/OrdersController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using SuperShop.Data;
+using SuperShop.Models;
 using System.Threading.Tasks;
 
 namespace SuperShop.Controllers
@@ -10,15 +11,35 @@ namespace SuperShop.Controllers
     public class OrdersController : Controller
     {
         private readonly IOrderRepository _orderRepository;
-        public OrdersController(IOrderRepository orderRepository) 
+        private readonly IProductRepository _productRepository;
+        public OrdersController(IOrderRepository orderRepository, IProductRepository productRepository) 
         {
             _orderRepository = orderRepository;
+            _productRepository = productRepository;
         }
 
         public async Task<IActionResult> Index()
         {
             var model = await _orderRepository.GetOrderAsync(this.User.Identity.Name);
                 return View(model);
+        }
+
+        public async Task<IActionResult> Create()
+        {
+            var model = await _orderRepository.GetDetailTempsAsync(this.User.Identity.Name);
+            return View(model);
+        }
+
+        public IActionResult AddProduct()
+        {
+            var model = new AddItemViewModel
+            {
+                Quantity = 1,
+                Products = (System.Collections.Generic.IEnumerable<Microsoft.AspNetCore.Mvc.Rendering.SelectListItem>)
+                _productRepository.GetComboProducts()
+            };
+
+            return View(model);
         }
     }
 }
--- SuperShop/Data/IProductRepository.cs
+++ SuperShop/Data/IProductRepository.cs
@@ -1,11 +1,15 @@
 ﻿using SuperShop.Data.Entities;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Web.Mvc;
 
 namespace SuperShop.Data
 {
     public interface IProductRepository : IGenericRepository<Product>
     {
         public IQueryable GetAllWithUsers();
+
+        IEnumerable<SelectListItem> GetComboProducts();
     }
 }
