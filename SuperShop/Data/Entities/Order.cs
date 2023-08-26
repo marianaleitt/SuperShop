6dfdf896a33c909a9716ee89d8505bd57b68cdf1
marianaleitt <marianaleitt@gmail.com>
2023-08-26 22:02:54 +0100

    modal windows

--- SuperShop/Data/Entities/Order.cs
+++ SuperShop/Data/Entities/Order.cs
@@ -39,7 +39,7 @@ namespace SuperShop.Data.Entities
 
 
         [Display(Name = "Order date")]
-        [DisplayFormat(DataFormatString = "{0:yyy/mm/dd hh:mm tt}", ApplyFormatInEditMode = false)]
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy HH:mm }", ApplyFormatInEditMode = false)]
         public DateTime? OrderDateLocal => this.OrderDate == null ?  null : this.OrderDate.ToLocalTime();
     }
 }
